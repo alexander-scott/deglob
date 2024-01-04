a38f5158386782339d4a60b4655ae73c7a4b261d
Alexander Scott <alexanderscott@outlook.com>
2024-01-04 15:38:27 +0000

    Add assert package for better test assertions

--- test/scenario_test.go
+++ test/scenario_test.go
@@ -1,8 +1,9 @@
 package test
 
 import (
-	"slices"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 
 	deglob "github.com/alexander-scott/deglob/internal"
 )
@@ -26,24 +27,7 @@ func TestScenarios(t *testing.T) {
 			actualFileContents := deglob.ProcessFile(pathToWorkspace + test + pathToBuildFileInScenario)
 
 			// Assert
-			if !slices.Equal(expectedFileContents, actualFileContents) {
-				t.Errorf("Slices are not equivalent.\nexpectedFileContents: \n%s\nactualFileContents: \n%s\n", difference(expectedFileContents, actualFileContents), difference(actualFileContents, expectedFileContents))
-				t.Error(actualFileContents)
-			}
+			assert.Equal(t, expectedFileContents, actualFileContents)
 		})
 	}
-}
-
-func difference(a, b []string) []string {
-	mb := make(map[string]struct{}, len(b))
-	for _, x := range b {
-		mb[x] = struct{}{}
-	}
-	var diff []string
-	for _, x := range a {
-		if _, found := mb[x]; !found {
-			diff = append(diff, x)
-		}
-	}
-	return diff
 }
