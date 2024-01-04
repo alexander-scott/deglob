fa5ab271af77d1f49b4512a5d4708f368b4d4980
Alexander Scott <alexanderscott@outlook.com>
2024-01-04 11:54:15 +0000

    Create initial scenario_2

---
+++ test/example_workspace/scenario_1/README.md
@@ -0,0 +1,3 @@
+### Scenario 1
+
+- Exclusive glob for a single srcs/hdrs attribute on a cc_library target that matches multiple cpp files
---
+++ test/example_workspace/scenario_2/main.cpp
@@ -0,0 +1,12 @@
+// Your First C++ Program
+
+#include <iostream>
+#include "file_1.cpp"
+#include "file_11.cpp"
+#include "file_2.cpp"
+#include "file_22.cpp"
+
+int main() {
+    std::cout << "Hello World!";
+    return 0;
+}
