2c2cbd6a47f78066e3b9587df7c1c2ec8d626000
Alexander Scott <alexanderscott@outlook.com>
2024-01-04 11:34:10 +0000

    Parameterise scenario tests

--- test/scenario_test.go
+++ test/scenario_test.go
@@ -7,18 +7,41 @@ import (
 	deglob "github.com/alexander-scott/deglob/internal"
 )
 
-func TestScenario1(t *testing.T) {
-	// Arrange
-	pathToWorkspace := "example_workspace/"
-	pathToBuildFile := pathToWorkspace + "scenario_1/BUILD"
-	pathToExpectedBuildFile := pathToWorkspace + "scenario_1/BUILD_UPDATED"
-	expectedFileContents := deglob.LoadFileContentsIntoMemory(pathToExpectedBuildFile)
-
-	// Act
-	updatedFileContents := deglob.ProcessFile(pathToBuildFile)
-
-	// Assert
-	if !slices.Equal(expectedFileContents, updatedFileContents) {
-		t.Fatalf("Are not equivalent %s", updatedFileContents)
+func TestScenarios(t *testing.T) {
+	tests := []string{
+		"scenario_1",
 	}
+
+	const pathToWorkspace = "example_workspace/"
+	const pathToBuildFileInScenario = "/BUILD"
+	const pathToExpectedBuildFile = "/EXPECTED.BUILD"
+
+	for _, test := range tests {
+		t.Run(test, func(t *testing.T) {
+			// Arrange
+			expectedFileContents := deglob.LoadFileContentsIntoMemory(pathToWorkspace + test + pathToExpectedBuildFile)
+
+			// Act
+			actualFileContents := deglob.ProcessFile(pathToWorkspace + test + pathToBuildFileInScenario)
+
+			// Assert
+			if !slices.Equal(expectedFileContents, actualFileContents) {
+				t.Errorf("Slices are not equivalent.\nexpectedFileContents: \n%s\nactualFileContents: \n%s\n", difference(expectedFileContents, actualFileContents), difference(actualFileContents, expectedFileContents))
+			}
+		})
+	}
+}
+
+func difference(a, b []string) []string {
+	mb := make(map[string]struct{}, len(b))
+	for _, x := range b {
+		mb[x] = struct{}{}
+	}
+	var diff []string
+	for _, x := range a {
+		if _, found := mb[x]; !found {
+			diff = append(diff, x)
+		}
+	}
+	return diff
 }
