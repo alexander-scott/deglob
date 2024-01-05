485d351c31f794b0ef5ffe1f9efa28cf79f7874a
Alexander Scott <alexanderscott@outlook.com>
2024-01-05 12:22:30 +0000

    Change scenario 2 to include files in subdir

--- test/example_workspace/scenario_2/main.cpp
+++ test/example_workspace/scenario_2/main.cpp
@@ -1,9 +1,9 @@
 // Your First C++ Program
 
 #include <iostream>
-#include "file_1.h"
-#include "file_2.cpp"
-#include "file_22.cpp"
+#include "src/file_1.h"
+#include "src/file_2.cpp"
+#include "src/file_22.cpp"
 
 int main() {
     std::cout << get_hello_world_string();
